7b93310b10351d47146f02896ed4bcaad9e651de
JTEP9U3 <amgoing25@gmail.com>
2025-06-02 20:08:07 +0900

    Create CardImageHighLow.java

---
+++ CardImageHighLow.java
@@ -0,0 +1,6 @@
+package project; //仮で「project」と記載
+import javax.swing.*; //Swingライブラリのすべてのクラスをインポート
+import java.awt.*; //AWTのすべてのクラスをインポート,レイアウトや描画に関するクラス
+import java.awt.event.*; //AWTのイベント関連クラスをすべてインポート,ユーザーの操作用のイベントクラス等
+import java.util.*; //Javaのユーティリティクラスをすべてインポート
+import java.util.List; //java.utilのListだけをインポート
